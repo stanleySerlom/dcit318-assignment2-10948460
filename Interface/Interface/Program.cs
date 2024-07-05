a7c1f992402fb720ab0fa3cb8a1c0b834c734280
hp <hp@KOBBYFLEX>
2024-07-05 17:48:47 +0000

    completed interface

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface { 
+// Define the IMovable interface
+interface  IMovable
+{
+    void Move();
+}
+
+// Car class implementing IMovable
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Bicycle class implementing IMovable
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Interfaces Example");
+
+        // Create instances of Car and Bicycle
+        IMovable car = new Car();
+        IMovable bicycle = new Bicycle();
+
+        // Call Move() method on each instance
+        Console.WriteLine("Moving the car:");
+        car.Move();
+
+        Console.WriteLine("\nMoving the bicycle:");
+        bicycle.Move();
+    }
+}
+}
+
+   
