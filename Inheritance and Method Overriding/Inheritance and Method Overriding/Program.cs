c36cc73c25a2e0367a7d789ecaa9eda2ecbcce6c
hp <hp@KOBBYFLEX>
2024-07-05 17:48:18 +0000

    completed inheritance

---
+++ Inheritance and Method Overriding/Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_and_Method_Overriding { 
+// Base class
+class Animal 
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Derived class Dog
+class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Derived class Cat
+class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Inheritance and Method Overriding Example");
+
+        // Creating instances of each class
+        Animal genericAnimal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        // Calling MakeSound() on each instance
+        Console.WriteLine("Generic Animal sound:");
+        genericAnimal.MakeSound();
+
+        Console.WriteLine("\nDog sound:");
+        dog.MakeSound();
+
+        Console.WriteLine("\nCat sound:");
+        cat.MakeSound();
+    }
+}
+}
+
+
+
